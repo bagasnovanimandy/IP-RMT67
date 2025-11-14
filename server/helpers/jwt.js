348b023d1ff54e7f67eee8c79a8aae8be4572bf8
bagasnovanimandy <bagas.novan.imandy@gmail.com>
2025-11-14 08:37:34 +0700

    refactor(config): update config.json and jwt.js

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "galindo_car_rental_secret";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function signToken(payload) {
   return jwt.sign(payload, JWT_SECRET);
